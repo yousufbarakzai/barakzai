332403ebda3c4de14f65481c9a0c13f026c63f9b
yousufbarakzai <yousufwwe20@gmail.com>
2025-10-28 15:39:39 +0430

    Add files via upload

---
+++ amazonshop/store/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.CharField(max_length=500, default='https://via.placeholder.com/300')
+    
+    def _str_(self):
+        return self.name
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
